3a8c58a6c70a7cd7195d1fd1806cb3fdcc7a208b
Sean Breckenridge <seanbrecke@gmail.com>
2020-09-04 01:58:33 -0700

    add skype module

---
+++ my/skype.py
@@ -0,0 +1,69 @@
+"""
+Parse Message Dates from Skypes GDPR JSON export
+"""
+
+# Isn't a lot of data here, seems a lot of the old
+# data is gone. Only parses a couple messages, might
+# as well use the datetimes for context on when I
+# was using skype
+
+# see https://github.com/seanbreckenridge/dotfiles/blob/master/.config/my/my/config/__init__.py for an example
+from my.config import skype as user_config  # type: ignore
+
+from dataclasses import dataclass
+
+from .core import Paths
+
+
+@dataclass
+class skype(user_config):
+    # path[s]/glob to the skype JSON files
+    export_path: Paths
+
+
+from .core.cfg import make_config
+config = make_config(skype)
+
+#######
+
+import json
+from pathlib import Path
+from typing import Sequence
+
+from .core import get_files
+
+
+def inputs() -> Sequence[Path]:
+    return get_files(config.export_path)
+
+
+from datetime import datetime
+from typing import Iterable
+from itertools import chain
+
+import dateparser
+
+from .core.common import LazyLogger
+logger = LazyLogger(__name__)
+
+
+Results = Iterable[datetime]
+
+
+def timestamps(from_paths=inputs) -> Results:
+    yield from chain(*map(_parse_file, from_paths()))
+
+
+def _parse_file(post_file: Path) -> Results:
+    with post_file.open('r') as pf:
+        items = json.load(pf)
+    for conv in items["conversations"]:
+        for msg in conv["MessageList"]:
+            yield dateparser.parse(msg["originalarrivaltime"].rstrip("Z"))
+
+
+def stats():
+    from .core import stat
+    return {
+        **stat(timestamps)
+    }
