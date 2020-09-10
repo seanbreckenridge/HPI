475316f22e887194584ade5dc4865e47406a76e8
Sean Breckenridge <seanbrecke@gmail.com>
2020-09-09 21:45:37 -0700

    filter duplicate zsh histories pre-emptively

--- my/zsh.py
+++ my/zsh.py
@@ -4,6 +4,8 @@ Parses ZSH history (uses exports from ./job/zsh_history.job) and current zsh his
 
 # if on multiple computers, the zsh histories can be copied into the zsh.export_path
 # and it will merge everything without duplicates
+# note: the first 100 lines of the zsh history should be different (even if only by one line), since this
+# removes duplicate histories by matching the first 100 lines (see filter_inputs below)
 
 # look at https://github.com/bamos/zsh-history-analysis
 
@@ -12,6 +14,7 @@ from my.config import zsh as user_config  # type: ignore
 
 from dataclasses import dataclass
 from typing import Optional
+from functools import lru_cache
 
 from .core import PathIsh, Paths
 
@@ -80,8 +83,67 @@ Results = Iterator[Entry]
 
 
 def history(from_paths=inputs) -> Results:
-    yield from _merge_histories(*map(_parse_file, from_paths()))
+    yield from _merge_histories(
+        *map(_parse_file,
+             filter_inputs(from_paths())
+        )
+    )
 
+
+
+@lru_cache
+def filesize(p: Path) -> int:
+    return p.lstat().st_size
+
+
+@lru_cache
+@listify
+def read_prefix(p: Path) -> Sequence[str]:
+    """
+    Read the first 100 lines of a file
+    """
+    lc = 0
+    with p.open('r') as f:
+        for line in f:
+            lc += 1
+            if lc > 100: break
+            yield line
+
+
+@lru_cache
+def prefix_lines_match(p1: Path, p2: Path) -> bool:
+    """
+    The first 100 lines of these 2 files match, and size of p1 > p2
+    This means we can ignore p2
+    """
+    if read_prefix(p1) == read_prefix(p2):
+        return filesize(p1) > filesize(p2)
+    return False
+
+
+# this improves latency by about 300ms for every backup
+# if you had 10 backups, this reduces the time from
+# 4 seconds, to 500ms, by preemptively filtering
+# duplicates, instead of reading the all in (which uses regex) and
+# then removing duplicates
+@listify
+def filter_inputs(paths: Sequence[Path]) -> Sequence[Path]:
+    # if a history file starts with the same 100 lines as another one
+    # and one file is bigger than another, keep the bigger file
+    sorted_files: List[Path] = sorted(paths, key=lambda p: p.lstat().st_size, reverse=True)
+    unique_files: Path = [sorted_files.pop(0)]
+    yield unique_files[0]  # largest file should always be returned
+    # for each file were unsure of whether or not its a duplicate
+    for s in sorted_files:
+        for u in list(unique_files):
+            # if the file we havent checked yet,
+            # matches a prefix of one of the unique files,
+            # ignore it
+            if prefix_lines_match(u, s):  # trying to remove 's' from yielded set
+                continue
+            else:
+                unique_files.append(s)
+                yield s  # this file didnt match, so it maybe from another computer/timeframe
 
 def stats():
     from .core import stat
@@ -101,10 +163,6 @@ def _merge_histories(*sources: Results) -> Results:
         emitted.add(key)
 
 
-# TODO: maybe cachew? sort of interferes with the live_zsh_history exports
-# if this gets to slow in the future, better solution would probably be to
-# pre-emptively read files in, check if ones a substring of another and
-# exclude it
 def _parse_file(histfile: Path) -> Results:
     dt, dur, command = None, None, ""
     # cant parse line by line since some commands are multiline
