ddec421927ac82825fc29ff836368e4c12f4bbcb
Sean Breckenridge <seanbrecke@gmail.com>
2020-09-09 22:36:27 -0700

    lint

--- my/zsh.py
+++ my/zsh.py
@@ -13,7 +13,7 @@ Parses ZSH history (uses exports from ./job/zsh_history.job) and current zsh his
 from my.config import zsh as user_config  # type: ignore
 
 from dataclasses import dataclass
-from typing import Optional
+from typing import Optional, List
 from functools import lru_cache
 
 from .core import PathIsh, Paths
@@ -83,12 +83,7 @@ Results = Iterator[Entry]
 
 
 def history(from_paths=inputs) -> Results:
-    yield from _merge_histories(
-        *map(_parse_file,
-             filter_inputs(from_paths())
-        )
-    )
-
+    yield from _merge_histories(*map(_parse_file, filter_inputs(from_paths())))
 
 
 @lru_cache
@@ -98,15 +93,16 @@ def filesize(p: Path) -> int:
 
 @lru_cache
 @listify
-def read_prefix(p: Path) -> Sequence[str]:
+def read_prefix(p: Path) -> Sequence[str]:  # type: ignore
     """
     Read the first 100 lines of a file
     """
     lc = 0
-    with p.open('r') as f:
+    with p.open("r") as f:
         for line in f:
             lc += 1
-            if lc > 100: break
+            if lc > 100:
+                break
             yield line
 
 
@@ -127,11 +123,13 @@ def prefix_lines_match(p1: Path, p2: Path) -> bool:
 # duplicates, instead of reading the all in (which uses regex) and
 # then removing duplicates
 @listify
-def filter_inputs(paths: Sequence[Path]) -> Sequence[Path]:
+def filter_inputs(paths: Sequence[Path]) -> Sequence[Path]:  # type: ignore
     # if a history file starts with the same 100 lines as another one
     # and one file is bigger than another, keep the bigger file
-    sorted_files: List[Path] = sorted(paths, key=lambda p: p.lstat().st_size, reverse=True)
-    unique_files: Path = [sorted_files.pop(0)]
+    sorted_files: List[Path] = sorted(
+        paths, key=lambda p: p.lstat().st_size, reverse=True
+    )
+    unique_files: List[Path] = [sorted_files.pop(0)]
     yield unique_files[0]  # largest file should always be returned
     # for each file were unsure of whether or not its a duplicate
     for s in sorted_files:
@@ -144,6 +142,7 @@ def filter_inputs(paths: Sequence[Path]) -> Sequence[Path]:
             else:
                 unique_files.append(s)
                 yield s  # this file didnt match, so it maybe from another computer/timeframe
+
 
 def stats():
     from .core import stat
