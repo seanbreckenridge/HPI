14a282bfd5351644217a513e73f05b29dcb7a625
Sean Breckenridge <seanbrecke@gmail.com>
2020-09-26 18:54:08 -0700

    fix function name

--- tests/test_window_watcher.py
+++ tests/test_window_watcher.py
@@ -1,7 +1,7 @@
 from my.window_watcher import history, Entry
 
 
-def test_ttt():
+def test_window_watcher():
     items = list(history())
     assert len(items) > 5
     assert isinstance(items[0], Entry)
