c8dcea10c8303c8624d3b16ba53b484f6c254714
Sean Breckenridge <seanbrecke@gmail.com>
2020-09-28 13:27:13 -0700

    add stats to food module

--- my/food/__init__.py
+++ my/food/__init__.py
@@ -2,3 +2,12 @@
 from ..body import prompt
 from .water import Water, water
 from .calories import food, Food
+
+
+def stats():
+    from ..core import stat
+
+    return {
+        **stat(food),
+        **stat(water),
+    }
