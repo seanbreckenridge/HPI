f33d44db5dd9d6c4bcc9825ab2c2f1e70ea4dde9
Sean Breckenridge <seanbrecke@gmail.com>
2020-11-05 17:27:38 -0800

    browsing: allow history/inputs to accept tmp_dir

--- my/browsing.py
+++ my/browsing.py
@@ -25,7 +25,7 @@ config = make_config(firefox)
 import os
 import tempfile
 from pathlib import Path
-from typing import Iterator, Sequence
+from typing import Iterator, Sequence, Optional
 
 from .core import Stats
 from .core.common import listify, get_files
@@ -47,31 +47,35 @@ from ffexport.save_hist import backup_history
 
 
 @listify
-def inputs() -> Sequence[Path]:  # type: ignore[misc]
+def inputs(tmp_dir: Optional[str] = None) -> Sequence[Path]:  # type: ignore[misc]
     """
     Returns all inputs, including old sqlite backups (from config.export_path) and the current firefox history
     """
     yield from get_files(config.export_path)
     # get the live file from ~/.mozilla/.... (see ffexport.save_hist)
     # warning: could run out of space on /tmp if your computer is up *forever*, or youre running pytest a bunch
-    tmp_dir: str = tempfile.mkdtemp()
+    if tmp_dir is None:
+        tmp_dir = tempfile.mkdtemp()
     # I only use the one profile, so profile defaults to *
     backup_history(browser="firefox", to=Path(tmp_dir))
     live_copy: Sequence[Path] = get_files(tmp_dir, glob="*.sqlite")
-    assert len(live_copy) == 1, f"Couldn't find live history backup in {tmp_dir}"
-    yield live_copy[0]
+    assert len(live_copy) >= 1, f"Couldn't find live history backup in {tmp_dir}"
+    yield sorted(live_copy, key=lambda p: p.stat().st_mtime)[-1]
 
 
 Results = Iterator[Visit]
 
 
-def history(from_paths=inputs) -> Results:
+def history(from_paths=inputs, tmp_dir: Optional[str] = None) -> Results:
     """
     used like:
     import my.browsing
     visits = list(my.browsing.history())
     """
-    yield from read_and_merge(*from_paths())
+    if hash(from_paths) == hash(inputs):
+        yield from read_and_merge(*from_paths(tmp_dir=tmp_dir))
+    else:
+        yield from read_and_merge(*from_paths())
 
 
 def stats() -> Stats:
