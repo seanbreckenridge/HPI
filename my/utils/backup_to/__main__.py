6002884a01f446f308b598a8354082d5a0325fd6
Sean Breckenridge <seanbrecke@gmail.com>
2021-05-19 17:33:57 -0700

    remove phone data dir
    have enough space
    on my phone to sync
    the entire ~/data
    directory now

--- my/utils/backup_to/__main__.py
+++ my/utils/backup_to/__main__.py
@@ -9,17 +9,9 @@ import click
 BASE_PREFIX: Path = Path(environ.get("HPIDATA", path.join(environ["HOME"], "data")))
 
 
-def _on_android() -> bool:
-    return shutil.which("termux-setup-storage") is not None
-
 
 def get_dir(name: str) -> Path:
-    base: Path
-    if _on_android():
-        base = BASE_PREFIX / "phone"
-    else:
-        base = BASE_PREFIX
-    to = (base / name).absolute()
+    to = (BASE_PREFIX / name).absolute()
     to.mkdir(parents=True, exist_ok=True)
     return to
 
@@ -29,10 +21,6 @@ def get_dir(name: str) -> Path:
 def main(name: str) -> None:
     """
     Helper script to locate a directory to backup to
-    while on an operating system (computer vs. phone)
-
-    Typically this backs up to ~/data/dirname on my computer
-    ~/data/phone/dirname on my phone
     """
     click.echo(str(get_dir(name)))
 
