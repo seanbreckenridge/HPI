28882c1666eeb1d9613df89ca832087f5a614d86
Sean Breckenridge <seanbrecke@gmail.com>
2021-09-23 12:38:46 -0700

    sync shell functions using hpi query

---
+++ functions.sh
@@ -0,0 +1,49 @@
+# These should work with both bash and zsh
+#
+# these use a bunch of common-ish shell tools
+# to interact with the hpi query JSON API
+# jq: https://github.com/stedolan/jq
+# fzf: https://github.com/junegunn/fzf
+
+alias mpv-from-stdin='mpv --playlist=- --no-audio-display --msg-level=file=error'
+
+#############
+# my.albums
+#############
+
+# how many albums I have on my list that I havent listened to yet
+alias albums-left='hpi query my.albums.to_listen | jq length'
+# pipe a list of album blobs to this to describe them
+albums-describe() {
+	jq -r '"\(.album_name) - \(.cover_artists) (\(.year))"'
+}
+albums-describe-score() {
+	jq -r '"[\(.score) | \(.listened_on)] \(.album_name) - \(.cover_artists) (\(.year))"'
+}
+# any albums which I can't find/have to order physical copies for to listen to
+alias albums-cant-find="hpi query -s my.albums._albums_cached | jq -r 'select(.note==\"cant find\")' | albums-describe"
+# list any albums I have yet to listen to, sorted by how many awards they've won
+albums-awards() {
+	local COUNT="${1:-10}"
+	hpi query my.albums.to_listen | jq -r "sort_by(.reasons | length) | reverse | .[0:${COUNT}] | .[] | \"[\(.reasons | length)] \(.album_name) - \(.cover_artists) (\(.year))\""
+}
+# just the next albums I should listen to chronologically
+albums-next() {
+	hpi query my.albums.to_listen -s --limit "${1:-10}" | jq -r '"\(.year) | \(.album_name) - \(.cover_artists)"'
+}
+
+##########
+# my.mpv
+##########
+
+# functions to replay music I've listened to recently
+mpv-recent() {
+	hpi query my.mpv.history --order-type datetime --reverse -s --limit "${1:-1}"
+}
+mpv-recent-paths() {
+	mpv-recent "$1" | jq -r .path
+}
+alias replay='mpv-recent-paths | mpv-from-stdin'
+replay-recent() {
+	mpv-recent-paths "${1:-$LINES}" | fzf | mpv-from-stdin
+}
