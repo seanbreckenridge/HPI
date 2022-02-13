1fba8ccf2fe93d67198aaa77355cac5f5910e7ac
Sean Breckenridge <seanbrecke@gmail.com>
2022-02-12 22:03:11 -0800

    google_takeout: update to match new parsing

--- my/google_takeout.py
+++ my/google_takeout.py
@@ -16,8 +16,8 @@ from my.core import make_config, dataclass
 from my.core.common import Stats, LazyLogger, mcachew, get_files, Paths
 from my.core.structure import match_structure
 
-from google_takeout_parser.path_dispatch import TakeoutParser, Results
-from google_takeout_parser.merge import GoogleEventSet
+from google_takeout_parser.path_dispatch import TakeoutParser
+from google_takeout_parser.merge import GoogleEventSet, CacheResults
 
 # see https://github.com/seanbreckenridge/dotfiles/blob/master/.config/my/my/config/__init__.py for an example
 from my.config import google as user_config
@@ -49,37 +49,46 @@ def inputs() -> List[Path]:
     return list(get_files(config.takeout_path))
 
 
+EXPECTED = (
+    "My Activity",
+    "Chrome",
+    "Location History",
+    "Youtube",
+    "YouTube and YouTube Music",
+)
+
+
 def _cachew_depends_on() -> List[str]:
     return sorted([str(p) for p in inputs()])
 
 
-EXPECTED = "My Activity"
-
-
+# ResultsType is a Union of all of the models in google_takeout_parser
 @mcachew(depends_on=_cachew_depends_on, logger=logger, force_file=True)
-def events(disable_takeout_cache: bool = DISABLE_TAKEOUT_CACHE) -> Results:
+def events(disable_takeout_cache: bool = DISABLE_TAKEOUT_CACHE) -> CacheResults:
+    count = 0
     emitted = GoogleEventSet()
     # reversed shouldn't really matter? but logic is to use newer
     # takeouts if they're named according to date, since JSON Activity
     # is nicer than HTML Activity
     for path in reversed(inputs()):
-        with match_structure(path, expected=EXPECTED) as results:
+        with match_structure(path, expected=EXPECTED, partial=True) as results:
             for m in results:
                 # e.g. /home/sean/data/google_takeout/Takeout-1634932457.zip") -> 'Takeout-1634932457'
                 # means that zipped takeouts have nice filenames from cachew
                 cw_id, _, _ = path.name.rpartition(".")
-                event_itr: Results
-                if disable_takeout_cache:
-                    event_itr = TakeoutParser(m).parse()
-                else:
-                    event_itr = TakeoutParser(m, cachew_identifier=cw_id).cached_parse()
-                for event in event_itr:
+                # each takeout result is cached as well, in individual databases per-type
+                tk = TakeoutParser(m, cachew_identifier=cw_id, error_policy="drop")
+                for event in tk.parse(cache=not disable_takeout_cache):
+                    count += 1
                     if isinstance(event, Exception):
                         continue
                     if event in emitted:
                         continue
                     emitted.add(event)
-                    yield event
+                    yield event  # type: ignore[misc]
+    logger.debug(
+        f"HPI Takeout merge: from a total of {count} events, removed {count - len(emitted)} duplicates"
+    )
 
 
 def stats() -> Stats:
