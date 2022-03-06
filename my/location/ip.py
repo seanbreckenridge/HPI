4a87b6ad19a0307976c7e1951d3595a29becaa5c
Sean Breckenridge <seanbrecke@gmail.com>
2022-03-06 05:58:01 -0800

    location/ip: handle import errors for each ip module

--- my/location/ip.py
+++ my/location/ip.py
@@ -14,17 +14,9 @@ import ipgeocache
 
 from my.core import Json
 from my.core.common import Stats, mcachew, LazyLogger
+from my.core.source import import_source
 
 from .models import Location
-
-from ..facebook.gdpr import (
-    AdminAction,
-    UploadedPhoto,
-    events as facebook_events,
-    config as facebook_config,
-)
-from ..blizzard.gdpr import events as blizzard_events
-from ..discord.data_export import activity, _cachew_depends_on
 
 
 logger = LazyLogger(__name__, level="warning")
@@ -66,31 +58,51 @@ def ips() -> Iterator[IP]:
     yield from _from_discord()
 
 
-@mcachew(
-    depends_on=lambda: list(map(str, Path(facebook_config.gdpr_dir).rglob("*"))),
-    logger=logger,
-)
+@import_source(module_name="my.facebook.gdpr")
 def _from_facebook() -> Iterator[IP]:
-    for e in facebook_events():
-        if isinstance(e, AdminAction) or isinstance(e, UploadedPhoto):
-            if not isinstance(e, Exception):
-                yield IP(dt=e.dt, addr=e.ip)
+    from my.facebook.gdpr import (
+        AdminAction,
+        UploadedPhoto,
+        events as facebook_events,
+        config as facebook_config,
+    )
+
+    @mcachew(
+        depends_on=lambda: list(map(str, Path(facebook_config.gdpr_dir).rglob("*"))),
+        logger=logger,
+    )
+    def _facebook_ips() -> Iterator[IP]:
+        for e in facebook_events():
+            if isinstance(e, AdminAction) or isinstance(e, UploadedPhoto):
+                if not isinstance(e, Exception):
+                    yield IP(dt=e.dt, addr=e.ip)
+
+    yield from _facebook_ips()
 
 
-@mcachew(logger=logger)
+@import_source(module_name="my.blizzard.gdpr")
 def _from_blizzard() -> Iterator[IP]:
+    from my.blizzard.gdpr import events as blizzard_events
+
     for e in blizzard_events():
         if e.event_tag == "Activity History":
             yield IP(dt=e.dt, addr=e.metadata[-2])
 
 
-@mcachew(depends_on=_cachew_depends_on, logger=logger)
+@import_source(module_name="my.discord.data_export")
 def _from_discord() -> Iterator[IP]:
-    for a in activity():
-        if a.fingerprint.ip is not None:
-            # for some reason returns some IPs that are using the private address space??
-            if not ipaddress.ip_address(a.fingerprint.ip).is_private:
-                yield IP(dt=a.timestamp, addr=a.fingerprint.ip)
+
+    from my.discord.data_export import activity, _cachew_depends_on
+
+    @mcachew(depends_on=_cachew_depends_on, logger=logger)
+    def _discord_ips() -> Iterator[IP]:
+        for a in activity():
+            if a.fingerprint.ip is not None:
+                # for some reason returns some IPs that are using the private address space??
+                if not ipaddress.ip_address(a.fingerprint.ip).is_private:
+                    yield IP(dt=a.timestamp, addr=a.fingerprint.ip)
+
+    yield from _discord_ips()
 
 
 def stats() -> Stats:
