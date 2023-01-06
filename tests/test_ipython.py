cbb0b8ae3ae7a21d9dd903c53709d63779a38aaa
Sean Breckenridge <seanbrecke@gmail.com>
2023-01-06 15:18:05 -0800

    tests/ipython: mypy fix; annotate as untyped call

--- tests/test_ipython.py
+++ tests/test_ipython.py
@@ -4,13 +4,13 @@ from .common import data
 
 import pytest
 
-from IPython.core.history import HistoryAccessor  # type: ignore[import]
+from IPython.core.history import HistoryAccessor
 
 
 # https://github.com/ipython/ipython/issues/13666
 def accessor_works() -> bool:
     ipython_db = str(data("ipython.sqlite"))
-    hist = HistoryAccessor(hist_file=ipython_db)
+    hist = HistoryAccessor(hist_file=ipython_db)  # type: ignore[no-untyped-call]
     try:
         hist.get_last_session_id()
         return True
